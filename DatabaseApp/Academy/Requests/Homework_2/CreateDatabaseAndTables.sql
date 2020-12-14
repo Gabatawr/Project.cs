eb7b0680f24f71f9784913c27f3690b8df96c4a8
Gabatawr <gabatawr@gmail.com>
2020-12-14 20:00:19 +0200

    HW completed

---
+++ DatabaseApp/Academy/Requests/Homework_2/CreateDatabaseAndTables.sql
@@ -0,0 +1,64 @@
+﻿use [master];
+go
+
+if db_id('AcademyDatabase') is not null
+begin
+    alter database [AcademyDatabase] set single_user with rollback immediate;
+    drop database [AcademyDatabase];
+end
+go
+
+create database [AcademyDatabase] 
+on 
+( 
+    NAME = N'AcademyDatabase_Data', 
+    FILENAME = N'E:\Database\Academy\AcademyDatabase_Data.mdf'
+)
+log on 
+(
+    NAME = N'AcademyDatabase_Log',
+    FILENAME = N'E:\Database\Academy\AcademyDatabase_Log.ldf'
+);
+go
+
+use [AcademyDatabase];
+go
+
+create table [Departments]
+(
+    [Id] int not null identity(1, 1) primary key,
+    [Financing] money not null check ([Financing] >= 0.0),
+    [Name] nvarchar(100) not null unique check ([Name] <> N'')
+);
+go
+
+create table [Faculties]
+(
+    [Id] int not null identity(1, 1) primary key,
+    [Dean] nvarchar(max) not null check ([Dean] <> N''),
+    [Name] nvarchar(100) not null unique check ([Name] <> N''),
+);
+go
+
+create table [Groups]
+(
+    [Id] int not null identity(1, 1) primary key,
+    [Name] nvarchar(10) not null unique check ([Name] <> N''),
+    [Rating] float not null check ([Rating] between 0 and 5),
+    [Year] int not null check ([Year] between 1 and 5)
+);
+go
+
+create table [Teachers]
+(
+    [Id] int not null identity(1, 1) primary key,
+    [EmploymentDate] date not null check ([EmploymentDate] >= '1990-01-01'),
+    [IsAssistant] bit not null default 0,
+    [IsProfessor] bit not null default 0,
+    [Name] nvarchar(max) not null check ([Name] <> N''),
+    [Position] nvarchar(max) not null check ([Position] <> N''),
+    [Premium] money not null check ([Premium] >= 0.0) default 0.0,
+    [Salary] money not null check ([Salary] > 0.0),
+    [Surname] nvarchar(max) not null check ([Surname] <> N'')
+);
+go
---
+++ DatabaseApp/Academy/Requests/Homework_2/InsertRandomValuesInTables.sql
@@ -0,0 +1,108 @@
+﻿truncate table [Departments] --------------------------------------------------
+declare 
+    @minFin int = 5000, 
+    @maxFin int = 45001 -- max 50_000
+
+insert into [Departments] ([Financing], [Name])
+                   values (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Software Development'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Data Science'),
+
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Аналітичної хімії та аналітичної токсикології'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Аптечної технології ліків'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Біологічної хімії'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Біотехнології'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Ботаніки'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Ветеринарної медицини та фармації'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Заводської технології ліків'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Іноземних мов'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Клінічної лабораторної діагностики'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Клінічної фармакології та клінічної фармації'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Косметології і аромології'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Медичної хімії'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Мікробіології, вірусології та імунології'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Менеджменту та публічного адміністрування'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Неорганічної та фізичної хімії'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Нормальної та патологічної фізіології'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Організації та економіки фармації'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Органічної хімії'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Освітніх та iнформаційних технологій'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Соціальної фармації'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Технології ліків'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Технології фармацевтичних препаратів'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Товарознавства'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Управління, економіки та забезпечення якості у фармації'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Фармацевтичного менеджменту та маркетингу'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Фармакології та фармакотерапії'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Фармакогнозії'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Фармацевтичної хімії'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Фізичної реабілітації та здоров’я'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Філософії та соціології'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Хімії природних сполук і нутриціології'),
+                          (cast(rand(checksum(newid())) * @maxFin as money) + @minFin, N'Фундаментальної та мовної підготовки');
+go --------------------------------------------------------------------------------
+
+truncate table [Faculties] --------------------------------------------------------
+insert into [Faculties] ([Name]                                               , [Dean])
+                 values (N'Computer Science'                                  , N'Kashyrin Artem Sergeevich'),
+
+                        (N'Фармацевтичний факультет'                          , N'Кузнєцова Вікторія Юріївна'),
+                        (N'Факультет фармацевтичних технологій та менеджменту', N'Живора Наталія Василівна'),
+                        (N'Факультет медико-фармацевтичних технологій'        , N'Набока Ольга Іванівна'),
+                        (N'Факультет з підготовки іноземних громадян'         , N'Калайчева Світлана Георгіївна');
+go --------------------------------------------------------------------------------
+
+truncate table [Groups] -----------------------------------------------------------
+declare 
+    @numberGroup int = cast(rand(checksum(newid())) * 400 as int) + 200,
+    @ratingGroup int,
+    @yearGroup int,
+
+@indexGroup int = 1
+while @indexGroup <= @numberGroup
+    begin
+        set @ratingGroup = cast(rand(checksum(newid())) * (5 + 1) as int)
+        set @yearGroup = cast(rand(checksum(newid())) * (4 + 1) as int) + 1
+
+        insert into [Groups] ([Name]                , [Rating]    , [Year])
+                      values (N'Группа ' + cast(@indexGroup as nvarchar(3)), @ratingGroup, @yearGroup)
+        set @indexGroup = @indexGroup + 1
+    end;
+go
+-----------------------------------------------------------------------------------
+
+truncate table [Teachers] ---------------------------------------------------------
+declare 
+    @numberTeacher int = cast(rand(checksum(newid())) * (120 + 1) as int) + 80, -- max 200
+    @isPro bit, @isAss bit, 
+    @salaryMinTeacher int = 200, @salaryMaxTeacher int = 2000,
+    @dateFrom date = '1990-01-01', @dateTo date = cast(current_timestamp as date),
+    @indexTeacher int = 1
+
+while @indexTeacher <= @numberTeacher
+    begin
+        if cast(rand(checksum(newid())) * (10 + 1) as int) <= 3
+            begin
+                set @isPro = 1 
+                set @isAss = 0
+            end
+        else
+            begin
+                set @isPro = 0 
+                set @isAss = 1
+            end
+
+        insert into [Teachers] ([Name], [Surname], [Position], [IsProfessor], [IsAssistant], [Salary], [Premium], [EmploymentDate])
+        values 
+        (
+            N'TeacherName ' + cast(@indexTeacher as nvarchar(3)),
+            N'TeacherSurname ' + cast(@indexTeacher as nvarchar(3)),
+            N'TeacherPosition ' + cast(@indexTeacher as nvarchar(3)),
+            @isPro, @isAss,
+            round(cast(rand(checksum(newid())) * @salaryMaxTeacher as money) + @salaryMinTeacher, 2),
+            round(cast(rand(checksum(newid())) * (1000 + 1) as money), 2),
+            dateadd(day, rand(checksum(newid()))*(1 + datediff(day, @dateFrom, @dateTo)), @dateFrom)
+        )
+
+        set @indexTeacher = @indexTeacher + 1
+    end;
+go ----------------------------------------------------------------------------
