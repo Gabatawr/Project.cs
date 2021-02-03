e334844bd2288f52a2ef1137e10140687ab46fde
Gabatawr <gabatawr@gmail.com>
2021-02-03 20:56:01 +0200

    lesson end

--- Barber/Barber/JournalForm.cs
+++ Barber/Barber/JournalForm.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using static Barber.Form1;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
 
 namespace Barber
 {
@@ -15,21 +17,75 @@ namespace Barber
         //---------------------------------------------------------------------
         private void JournalForm_Load(object sender, System.EventArgs e)
         {
-            List<DataGridView> views = new() { dataGridView1, dataGridView2, dataGridView3 };
-            foreach (var g in views)  g.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
-            //-------------------------------------------------
-            List<object> lists = new();
-            //-------------------------------------------------
+            //List<DataGridView> views = new() { dataGridView1, dataGridView2, dataGridView3 };
+            //foreach (var v in views)  v.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            ////-------------------------------------------------
+            //List<object> lists = new();
+            ////-------------------------------------------------
+            //using (BarberContext db = new((Owner as Form1).Connection.ConnectionString))
+            //{
+            //    lists.Add(db.Clients.ToList());
+            //    lists.Add(db.Gender.ToList());
+            //    lists.Add(db.Barbers.ToList());
+            //}
+            ////-------------------------------------------------
+            //for (int i = 0; i < lists.Count; i++) views[i].DataSource = lists[i];
+            ////-------------------------------------------------
+            //foreach (var v in views) v.Columns["Id"].ReadOnly = true;
+
             using (BarberContext db = new((Owner as Form1).Connection.ConnectionString))
             {
-                lists.Add(db.Clients.ToList());
-                lists.Add(db.Gender.ToList());
-                lists.Add(db.Barbers.ToList());
+                StringBuilder sb = new();
+                //-------------------------------------------------
+                //var q = from c in db.Clients
+                //        where c.Id < 3
+                //        select c;
+                //
+                //foreach(Client c in q.ToList())
+                //{
+                //    label1.Text += c.Id + " : "
+                //                + c.SurName + " " + c.Name + " " + c.SecName + " : "
+                //                + c.GenderId + " : "
+                //                + c.Phone + " : " + c.Email + '\n';
+                //}
+                //-------------------------------------------------
+                //var q = from c in db.Clients
+                //        join g in db.Gender on c.GenderId equals g.Id
+                //        where c.Id < 4 && g.Id < 10
+                //        orderby c.Id descending
+                //        select new
+                //        {
+                //            Id = c.Id,
+                //            Name = c.Name,
+                //            Gender = g.Name
+                //        };
+                //
+                //foreach (var a in q.ToList())
+                //{
+                //    sb.AppendJoin(" : ", a.Id, a.Name, a.Gender);
+                //    sb.AppendLine();
+                //}
+                //label1.Text = sb.ToString();
+                //-------------------------------------------------
+                foreach (var c in db.Clients.ToList())
+                {
+                    sb.Append(c.Name);
+                    sb.Append(" : ");
+                    try
+                    {
+                        sb.Append(db.Gender.Where(g => g.Id == c.GenderId)
+                                           .First()
+                                           .Name
+                        );
+                    }
+                    catch   { sb.Append('-');  }
+                    finally { sb.AppendLine(); }
+                }
+
+                label1.Text = sb.ToString();
+                //-------------------------------------------------
             }
-            //-------------------------------------------------
-            for (int i = 0; i < lists.Count; i++) views[i].DataSource = lists[i];
-            //-------------------------------------------------
-            foreach (var g in views) g.Columns["Id"].ReadOnly = true;
+
         }
         //---------------------------------------------------------------------
     }
