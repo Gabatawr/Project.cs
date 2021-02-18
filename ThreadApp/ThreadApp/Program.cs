f017de5be8c4f1130114cce889b5644489eb9498
Gabatawr <gabatawr@gmail.com>
2021-02-18 21:44:26 +0200

    lesson end

---
+++ ThreadApp/ThreadApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ThreadOne
+{
+    class PracticeOne
+    {
+        static Random rand = new();
+
+        static void MIN(object obj) => Console.WriteLine("Min: " + (obj as List<int>).Min());
+        static void AVG(object obj) => Console.WriteLine("Avg: " + (obj as List<int>).Average());
+        static void MAX(object obj) => Console.WriteLine("Max: " + (obj as List<int>).Max());
+
+        static void Main()
+        {
+            List<int> l = new();
+
+            int randMax = 100000;
+            int randMin = 1000;
+
+            for (int i = 0; i < 10000; i++)
+                l.Add(rand.Next(randMin, randMax + 1));
+
+            Thread[] tarr = { new(MIN), new(AVG), new(MAX) };
+            foreach (var t in tarr)
+                t.Start(l);
+
+            Console.ReadKey();
+        }
+    }
+}
