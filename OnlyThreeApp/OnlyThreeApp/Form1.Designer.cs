640e8fe189ea96eeb89a12c8a5d25d4e0ea19e20
Gabatawr <gabatawr@gmail.com>
2021-03-06 12:01:19 +0200

    task 3 completed

---
+++ OnlyThreeApp/OnlyThreeApp/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace OnlyThreeApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lbRunCounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbRunCounter
+            // 
+            this.lbRunCounter.AutoSize = true;
+            this.lbRunCounter.Location = new System.Drawing.Point(12, 9);
+            this.lbRunCounter.Name = "lbRunCounter";
+            this.lbRunCounter.Size = new System.Drawing.Size(13, 15);
+            this.lbRunCounter.TabIndex = 0;
+            this.lbRunCounter.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 33);
+            this.Controls.Add(this.lbRunCounter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbRunCounter;
+    }
+}
+
---
+++ OnlyThreeApp/OnlyThreeApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace OnlyThreeApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            Semaphore s;
+            string sName = Application.ProductName + nameof(Semaphore);
+            int msec = 5000;
+
+            try { s = Semaphore.OpenExisting(sName); }
+            catch (WaitHandleCannotBeOpenedException) { s = new(0, 3, sName); }
+
+            try { lbRunCounter.Text = (s.Release() + 1).ToString(); }
+            catch (SemaphoreFullException)
+            {
+                lbRunCounter.Text = "Only 3! App close after 5 sec..";
+                System.Threading.Timer t = new((e) => Application.Exit(), null, msec, msec);
+            }
+        }
+    }
+}
