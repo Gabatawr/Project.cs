d4db762e69234f632e4175f3248a43ebf1ce659b
Gabatawr <gabatawr@gmail.com>
2021-03-13 16:21:21 +0200

    hw2

---
+++ DemoChat/DemoChat_Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DemoChat_Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            IPEndPoint eP = new(IPAddress.Parse("127.0.0.1"), 8081);
+            Socket client = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                StringBuilder serverMsg = new();
+                var buff = new byte[256];
+                int bytes = 0;
+
+                client.Connect(eP);
+                while (true)
+                {
+                    Console.Write("Message : ");
+                    string msg = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(msg) is false)
+                    {
+                        client.Send(Encoding.ASCII.GetBytes(msg));
+                        do
+                        {
+                            bytes = client.Receive(buff);
+                            serverMsg.Append(Encoding.ASCII.GetString(buff, 0, bytes));
+                        } while (client.Available > 0);
+
+                        Console.WriteLine(serverMsg);
+                        serverMsg.Clear();
+                    }
+                    else Console.WriteLine("Massage empty..");
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e); }
+            finally { client.Close(); }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ DemoChat/DemoChat_Server/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DemoChat_Server
+{
+    class Program
+    {
+        static void Main()
+        {
+            IPEndPoint eP = new(IPAddress.Parse("127.0.0.1"), 8081);
+            Socket server = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            server.Bind(eP);
+            server.Listen(1);
+            Socket client = server.Accept();
+            try
+            {
+                StringBuilder clientMsg = new();
+                var buff = new byte[256];
+                int bytes = 0;
+
+                while (true)
+                {
+                    do
+                    {
+                        bytes = client.Receive(buff);
+                        clientMsg.Append(Encoding.ASCII.GetString(buff, 0, bytes));
+                    } while (client.Available > 0);
+
+                    string Massege(Socket s, string msg)
+                    {
+                        var dtn = DateTime.Now;
+                        return dtn.ToString("t")
+                        + " from "
+                        + $"[{s.LocalEndPoint}]"
+                        + $" : {msg}";
+                    }
+                    
+                    Console.WriteLine(Massege(client, clientMsg.ToString()));
+                    clientMsg.Clear();
+
+                    client.Send(Encoding.ASCII.GetBytes(Massege(client, "Message received")));
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e); }
+            finally
+            {
+                client.Close();
+                server.Close();
+            }
+        }
+    }
+}
